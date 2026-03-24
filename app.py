5fe86be1a7107e346244d81dee8c98cc115201b3
= <uday>
2026-03-24 20:55:30 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, render_template, redirect, url_for, session
+
+app = Flask(__name__)
+
+app.secret_key = "SAY_MY_NAME!"
+
+student_d = {}
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if username in student_d:
+            return "User already exists! <a href='/register'>Try again</a>"
+
+        student_d[username] = password
+        print(f"User Registered: {username}")
+
+        # After registering, go to the Login page
+        return redirect(url_for('login'))
+
+    return render_template("register.html")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if username in student_d and student_d[username] == password:
+            session['user'] = username
+            return redirect(url_for('dashboard'))
+        else:
+            return "Invalid credentials! <a href='/login'>Try again</a>"
+
+    return render_template("login.html")
+
+
+@app.route('/dashboard')
+def dashboard():
+    if 'user' in session:
+        current_student = session['user']
+        return render_template("dashboard.html", student=current_student)
+
+    return redirect(url_for('login'))
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('login'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
